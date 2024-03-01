2b63871c139984e0382900ea4fe4261fbc03c408
Timothe Viala <vlt4764a@ensfsi.onmicrosoft.com>
2024-03-01 08:48:50 +0100

    Méthode dans la classe Village pour ajouter et trouver les habitants

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,57 +1,58 @@
 package personnages;
 
 public class Gaulois {
-	//attribut
+	// attribut
 	private String nom;
 	private int force;
-	private int effetPotion=1;
-	
-	//constructeur
-	public Gaulois(String nom,int force) {
-		this.nom=nom;
-		this.force=force;
+	private int effetPotion = 1;
+
+	// constructeur
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
 	}
-	
-	
-	//methode
+
+	// methode
 	public String getNom() {
 		return nom;
 	}
+
 	public int getForce() {
 		return force;
 	}
-	
+
 	public void frapperRomain(Romain romain) {
-		System.out.println("Le gaulois "+nom+" envoie un grand coup dans la mâchoire de "+romain.getNom());
-		romain.recevoirCoup((force/3)*effetPotion);
-		
+		System.out.println("Le gaulois " + nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
+		romain.recevoirCoup((force / 3) * effetPotion);
+
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Gaulois [nom=" + nom + ", force=" + force + "]";
-	}	
-	
+		return nom;
+	}
+
 	public void parler(String texte) {
-		System.out.println(prendreParole()+"<<"+texte+">>");
+		System.out.println(prendreParole() + "<<" + texte + ">>");
 	}
+
 	private String prendreParole() {
-		return "Le gaulois "+ nom +" prend la parole: ";
+		return "Le gaulois " + nom + " prend la parole: ";
 	}
-	
+
 	public void boirePotion(int forcePotion) {
-		effetPotion=force*forcePotion;
-		parler("Merci Druide, je sens que ma force est "+forcePotion+" fois décuplée");
+		effetPotion = force * forcePotion;
+		parler("Merci Druide, je sens que ma force est " + forcePotion + " fois décuplée");
 	}
-	
+
 	public static void main(String[] args) {
-		Gaulois asterix=new Gaulois("asterix",8);
-		Romain minus=new Romain("minus",80);
+		Gaulois asterix = new Gaulois("asterix", 8);
+		Romain minus = new Romain("minus", 80);
 		asterix.boirePotion(11);
 		asterix.frapperRomain(minus);
 		asterix.parler("Coucou");
 		asterix.getNom();
-		
+
 	}
 
 }
--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -1,9 +1,75 @@
 package personnages;
 
 public class Village {
-	private String nom;
-	public Village(String nom) { this.nom = nom; }
-	public String getNom() { return nom; }
 
+	// attribut
+	private String nom;
+	private Chef chef;
+	private int nbVillageois = 0;
+	private Gaulois[] villageois;
+
+	// constructeur
+	public Village(String nom, int nbVillageoisMax) {
+		this.nom = nom;
+		this.villageois = new Gaulois[nbVillageoisMax];
+	}
+
+	// methode
+
+	public void ajouterHabitant(Gaulois gaulois) {
+		if (nbVillageois < villageois.length) {
+			villageois[nbVillageois] = gaulois;
+			nbVillageois += 1;
+		} else {
+			System.out.println("Village rempli!");
+
+		}
+	}
+
+	public Gaulois trouverHabitant(int placeDansVillage) {
+		return (villageois[placeDansVillage]);
+	}
+
+	public void afficherVillageois(Village village) {
+		System.out.println("Dans le village d'" + village.getChef() + " vivent les gaulois:");
+		for (int i = 0; i < villageois.length; i++) {
+			if (villageois[i] != null) {
+				System.out.println("-" + villageois[i]);
+			}
+
+		}
+
+	}
+
+	@Override
+	public String toString() {
+		return "Village [chef=" + chef + "]";
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setChef(Chef chef) {
+		this.chef = chef;
+	}
+
+	public Chef getChef() {
+		return chef;
+	}
+
+	public static void main(String args[]) {
+		Village village = new Village("Village des Irréductibles", 30);
+		// Gaulois gaulois=village.trouverHabitant(30);
+		Chef abraracourcix = new Chef("Abraracourcix", 8, village);
+		village.setChef(abraracourcix);
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		village.ajouterHabitant(asterix);
+		Gaulois gaulois = village.trouverHabitant(1);
+		System.out.println(gaulois);
+		Gaulois obelix = new Gaulois("Obélix", 25);
+		village.ajouterHabitant(obelix);
+		village.afficherVillageois(village);
+	}
 
 }
