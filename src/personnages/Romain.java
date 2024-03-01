89e6cae61e2a893a45a9cdff937680f2901d37d0
Timothe Viala <vlt4764a@ensfsi.onmicrosoft.com>
2024-03-01 08:59:48 +0100

    Invariant/variant pre/post cond

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,53 +1,56 @@
 package personnages;
 
 public class Romain {
-	//attribut
+	// attribut
 	private String nom;
 	private int force;
-		
-	//constructeur
-	public Romain(String nom,int force) {
-		this.nom=nom;
-		this.force=force;
-	}
-	
 
-	//methode
+	// constructeur
+	public Romain(String nom, int force) {
+		assert force >= 0;
+		this.nom = nom;
+		this.force = force;
+	}
+
+	// methode
 	public String getNom() {
 		return nom;
 	}
+
 	public int getForce() {
 		return force;
 	}
-	
+
 	public void parler(String texte) {
-		System.out.println(prendreParole()+"<<"+texte+">>");
+		System.out.println(prendreParole() + "<<" + texte + ">>");
 	}
+
 	private String prendreParole() {
-		return "Le romain "+ nom +" prend la parole: ";
+		return "Le romain " + nom + " prend la parole: ";
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Romain [nom=" + nom + ", force=" + force + "]";
 	}
 
-
 	public void recevoirCoup(int forceCoup) {
-	   force-=forceCoup;
-	   
-	   if (force>0) {
-		parler("Aïe");
-	} else {
-		parler("J'abandonne...");
-	}
-		
-	}
-public static void main(String[] args) {
-	Romain minus=new Romain("Minus",80);
-	minus.recevoirCoup(20);
+		assert force >= 0;
+		int forcVar = force;
+		force -= forceCoup;
+
+		if (force > 0) {
+			parler("Aïe");
+		} else {
+			parler("J'abandonne...");
+		}
+
+		assert forcVar > force;
 	}
 
-	
+	public static void main(String[] args) {
+		Romain minus = new Romain("Minus", 6);
+		minus.recevoirCoup(20);
+	}
 
 }
